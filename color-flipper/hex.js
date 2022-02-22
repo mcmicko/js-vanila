9722cf3f9274963f8ccb10c6c80672c9b3a0212a
mcmicko <mcmzrc@gmail.com>
2022-02-22 20:40:20 +0100

    Modification

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -1,14 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const circle1 = document.getElementById("circle1");
+const circle2 = document.getElementById("circle2");
 
 btn.addEventListener("click", () => {
   let hexColor = "#";
+  let hexColor2 = "#";
+  let hexColor3 = "#";
   for (let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()];
+    hexColor2 += hex[getRandomNumber()];
+    hexColor3 += hex[getRandomNumber()];
   }
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
+  circle1.style.backgroundColor = hexColor2;
+  circle2.style.backgroundColor = hexColor3;
 });
 
 const getRandomNumber = () => {
--- traversy-simple-window/main.js
+++ traversy-simple-window/main.js
@@ -2,6 +2,41 @@ const modal = document.getElementById("simpleModal");
 const modalBtn = document.getElementById("modalBtn");
 const closeBtn = document.querySelector(".closeBtn");
 
+const seeAllBtn = document.getElementById("see-all-btn");
+const searchBookInput = document.getElementById("search-book-input");
+const searchBookBtn = document.getElementById("search-book-btn");
+const bookNameInput = document.getElementById("book-name-input");
+const bookWritterInput = document.getElementById("book-writter-input");
+const additionalAttributeNameInput = document.getElementById(
+  "additional-attribute-name-input"
+);
+const additionalAttributeValueInput = document.getElementById(
+  "additional-attribute-value-input"
+);
+const addBookBtn = document.getElementById("add-book-btn");
+const deleteBookInput = document.getElementById("book-delete-input");
+const deleteBookBtn = document.getElementById("delete-book-btn");
+
+const itemList = document.getElementById("items");
+
+const books = [
+  {
+    id: 1,
+    name: "Misery",
+    writter: "Steven King",
+  },
+  {
+    id: 2,
+    name: "Special Mission",
+    writter: "Otto Skorzeny",
+  },
+  {
+    id: 3,
+    name: "Head Hunters",
+    writter: "Ju Nesbe",
+  },
+];
+
 modalBtn.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
 window.addEventListener("click", closeOutside);
@@ -17,3 +52,84 @@ function closeOutside(e) {
     modal.style.display = "none";
   }
 }
+
+const addBook = (name, writter, attributeName = "", attributeValue = "") => {
+  const addedBook = {
+    id: Math.floor(Math.random() * 100),
+    name,
+    writter,
+  };
+
+  const bookExist = books.some(
+    (b) =>
+      b.name.toLowerCase().trim().split(" ").join("") ===
+      addedBook.name.toLowerCase().trim().split(" ").join("")
+  );
+
+  if (bookExist) {
+    alert(`Book with provided name ${name.trim()} already exists!`);
+    return;
+  }
+
+  if (attributeName !== "" && attributeValue !== "") {
+    addedBook[attributeName] = attributeValue;
+  }
+
+  if (
+    !name.toLowerCase().trim().split(" ").join("") ||
+    !writter.toLowerCase().trim().split(" ").join("")
+  ) {
+    alert("you must provide some value");
+    return;
+  }
+
+  let li = document.createElement("li");
+
+  let delbtn = document.createElement("button");
+  delbtn.className = "del";
+  delbtn.appendChild(document.createTextNode("del"));
+
+  li.className = "list-group-item";
+
+  books.push(addedBook);
+  li.innerHTML = `${addedBook.name}`;
+  li.appendChild(delbtn);
+  itemList.appendChild(li);
+  modal.style.display = "none";
+};
+
+addBookBtn.addEventListener("click", () => {
+  addBook(
+    bookNameInput.value,
+    bookWritterInput.value,
+    additionalAttributeNameInput.value,
+    additionalAttributeValueInput.value
+  );
+});
+
+const removeItem = (e) => {
+  if (e.target.classList.contains("del")) {
+    if (confirm("Are you sure")) {
+      let li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+};
+
+itemList.addEventListener("click", removeItem);
+
+const filterItems = (e) => {
+  let text = e.target.value.toLowerCase();
+  let items = itemList.getElementsByTagName("li");
+
+  Array.from(items).forEach((item) => {
+    let itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) !== -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+};
+
+filter.addEventListener("input", filterItems);
